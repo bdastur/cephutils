07e5d8ee174474c49ab9eacc9643bc48105ee56d
Behzad Dastur <bdastur@gmail.com>
2016-04-16 01:00:05 +0000

    Initial cephlib commit, a wrapper on rados and rbd.

---
+++ cephlibs/cephlib.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import rados
+#import pprint
+
+
+
+class Cephlib(object):
+    '''
+    A wrapper lib for rados.
+    '''
+    def __init__(self, cephconf):
+        print "Cephlib init"
+        self.ceph_configfile = cephconf
+        try:
+            self.cluster = rados.Rados(conffile=cephconf)
+            self.cluster.connect()
+        except rados.Error as err:
+            print "Rados Error: connect failed [%s]" % err
+
+    def get_cluster_summary_json(self):
+        '''
+        Get the Cluster summary
+        '''
+
+        cmd = {"prefix": "status", "format": "json"}
+        try:
+            ret, buf, err = self.cluster.mon_command(json.dumps(cmd), "")
+        except rados.Error as err:
+            print "Rados Error: [get cluster summary] [%s]" % err
+            return None
+
+        return buf
+
+    def get_ceph_osd_stat_json(self):
+        '''
+        Get the osd stat output
+        '''
+        cmd = {"prefix": "osd stat", "format": "json"}
+        try:
+            ret, buf, err = self.cluster.mon_command(json.dumps(cmd), "")
+        except rados.Error as err:
+            print "Rados Error: [get osd stat] [%s]" % err
+            return None
+
+        return buf
+
+    def get_ceph_osd_count(self):
+        '''
+        Get the Total, up, in osds
+        '''
+        data = self.get_ceph_osd_stat_json()
+        dictdata = json.loads(data)
+        try:
+            (total_osds, up_osds, in_osds) = \
+                (dictdata['num_osds'],
+                 dictdata['num_up_osds'],
+                 dictdata['num_in_osds'])
+
+        except KeyError as err:
+            print "KeyError [%s] " % err
+            return (None, None, None)
+
+        return (int(total_osds), int(up_osds), int(in_osds))
+
+    def get_cluster_health(self):
+        '''
+        Get the clsuter health
+        '''
+        data = self.get_cluster_summary_json()
+        dictdata = json.loads(data)
+
+        try:
+            health_status = dictdata['health']['overall_status']
+        except KeyError as err:
+            print "KeyError [%s] " % err
+            return None
+
+        return health_status
+
+
+
+
